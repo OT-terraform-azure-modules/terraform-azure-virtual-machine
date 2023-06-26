306ba37041003bd331cbca90e0d0c7a4cf93f650
kulsinghdeep <93075914+kulsinghdeep@users.noreply.github.com>
2023-06-26 16:29:50 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "nic_id" {
   value       = azurerm_network_interface.nic.id
 }
 
+output "nic_name" {
+  value       = azurerm_network_interface.nic.name
+}
+
 
 output "vm_private_ip" {
   value       = azurerm_network_interface.nic.private_ip_address
