17f7b464ecd385c4678e4cfa443d3af365802758
kulsinghdeep <93075914+kulsinghdeep@users.noreply.github.com>
2023-06-28 14:47:50 +0530

    Update nic.tf

--- nic.tf
+++ nic.tf
@@ -1,3 +1,14 @@
+resource "azurerm_public_ip" "example" {
+  name                    = var.pubip_name
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+
+  tags = var.tagMap
+}
+
+
 resource "azurerm_network_interface" "nic" {
   name                = var.name
   location            = var.location
@@ -6,5 +17,6 @@ resource "azurerm_network_interface" "nic" {
     name                          = var.name
     subnet_id                     = var.nic_ip_configuration_subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = var.pubip_name == "" ? null : azurerm_public_ip.example.id 
   }
 }
